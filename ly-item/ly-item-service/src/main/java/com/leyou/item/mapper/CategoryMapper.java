938f6fa6a6e89a5f52f953d1af6f96629b0623d6
Chaocaijinbao <1175116526@qq.com>
2019-12-05 17:00:53 +0800

    2019-12-05 17:00

--- ly-item/ly-item-service/src/main/java/com/leyou/item/mapper/CategoryMapper.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/mapper/CategoryMapper.java
@@ -1,8 +1,15 @@
 package com.leyou.item.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
 import com.leyou.item.pojo.Category;
 import tk.mybatis.mapper.common.Mapper;
 
 
 public interface CategoryMapper extends Mapper<Category> {
+	
+	@Select("SELECT * FROM tb_category WHERE id IN (SELECT category_id FROM tb_category_brand WHERE brand_id = #{bid})")
+	List<Category> queryCategoryByBrandId(Long bid);
 }
